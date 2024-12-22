40a8278c91a55b286df71aa6ef87d2e1201753b1
kirumel <kirumel7621@gmail.com>
2024-12-22 17:30:44 +0900

    qrtest9

--- pages/api/scanQR.ts
+++ pages/api/scanQR.ts
@@ -7,18 +7,30 @@ export default async function handler(req: any, res: any) {
     try {
       const { qr, nameid } = req.body;
       const qrArray = ["sj1", "sj2", "sj3", "sj4", "sj5", "sj6", "sj7"];
-      if (!qrArray.includes(qr)) {
+      if (qrArray.includes(qr)) {
         res.status(202).json({
-          message: `이미 스켄한 ${qr.toString().slice(2)}번째 코드입니다`,
+          message: `허용되지 않는 코드입니다다`,
         });
       } else {
-        const post = await prisma.eventQR.create({
-          data: {
-            nameid: nameid,
+        const findDB = await prisma.eventQR.findMany({
+          where: {
             qrcode: qr,
+            nameid: nameid,
           },
         });
-        res.status(200).json({ message: qr.toString().slice(2) });
+        if (findDB.length > 0) {
+          res.status(202).json({
+            message: `이미 스켄된 ${qr.toString().slice(2)}번째 코드입니다`,
+          });
+        } else {
+          const post = await prisma.eventQR.create({
+            data: {
+              nameid: nameid,
+              qrcode: qr,
+            },
+          });
+          res.status(200).json({ message: qr.toString().slice(2) });
+        }
       }
     } catch (error) {
       res.status(202).json({ message: `오류발생${error} ` });
