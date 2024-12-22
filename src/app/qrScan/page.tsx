3d2eda4cfde4b6ee28ace5f0e04856701d70e073
kirumel <kirumel7621@gmail.com>
2024-12-22 16:43:07 +0900

    qrtest2

--- src/app/qrScan/page.tsx
+++ src/app/qrScan/page.tsx
@@ -18,7 +18,6 @@ export default function Page() {
       .getUserMedia({ video: true })
       .then((stream) => {
         console.log("카메라에 접근 성공", stream);
-        // 카메라 스트림 사용 코드
       })
       .catch((error) => {
         console.error("카메라 권한 거부", error);
@@ -44,56 +43,13 @@ export default function Page() {
   }
   return (
     <>
-      <ToastContainer
-        position="top-center"
-        autoClose={5000}
-        hideProgressBar
-        newestOnTop={true}
-        closeOnClick
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover={false}
-        theme="light"
-        transition={Slide}
-        closeButton={false}
-      />
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          marginBottom: "20px",
-        }}
-      >
-        <h3 style={{ marginBottom: "0px" }}>숨겨진 QR코드를 찾아라!</h3>
-        <p className="subtitle" style={{ fontSize: "10px", marginTop: "0px" }}>
-          힌트를 보고 메이커실에 숨겨진 qr코드를 찾아주세요!
-        </p>
-        <div>
-          <button>힌트보기</button>
-          <button>상품보기</button>
-        </div>
-      </div>
       <div className="scanner">
         <Scanner
+          components={{ finder: true }}
           onScan={(data) => {
             handleScan(data[0].rawValue);
           }}
         />
-      </div>
-      <div className="stemp">
-        <div className="stemp-box">
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>4</div>
-        </div>
-        <div className="stemp-box">
-          <div>5</div>
-          <div>6</div>
-          <div>7</div>
-        </div>
       </div>
     </>
   );
