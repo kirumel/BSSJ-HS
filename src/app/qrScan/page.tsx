2c009cdc0e463429c53e7c320c0dfcbe6eb980db
kirumel <kirumel7621@gmail.com>
2024-12-22 16:53:56 +0900

    qrtest5

--- src/app/qrScan/page.tsx
+++ src/app/qrScan/page.tsx
@@ -31,12 +31,58 @@ export default function Page() {
   }
   return (
     <>
-      <Scanner
-        components={{ finder: true }}
-        onScan={(data) => {
-          handleScan(data[0].rawValue);
-        }}
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar
+        newestOnTop={true}
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover={false}
+        theme="light"
+        transition={Slide}
+        closeButton={false}
       />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          marginBottom: "20px",
+        }}
+      >
+        <h3 style={{ marginBottom: "0px" }}>숨겨진 QR코드를 찾아라!</h3>
+        <p className="subtitle" style={{ fontSize: "10px", marginTop: "0px" }}>
+          힌트를 보고 메이커실에 숨겨진 qr코드를 찾아주세요!
+        </p>
+        <div>
+          <button>힌트보기</button>
+          <button>상품보기</button>
+        </div>
+      </div>
+      <div className="scanner">
+        <Scanner
+          components={{ finder: true }}
+          onScan={(data) => {
+            handleScan(data[0].rawValue);
+          }}
+        />
+      </div>
+      <div className="stemp">
+        <div className="stemp-box">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+        </div>
+        <div className="stemp-box">
+          <div>5</div>
+          <div>6</div>
+          <div>7</div>
+        </div>
+      </div>
     </>
   );
 }
