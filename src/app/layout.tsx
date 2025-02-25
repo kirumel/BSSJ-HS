cfad33beeacb25e43970b5b7e87de5984f235a88
kirumel <kirumel7621@gmail.com>
2025-02-26 01:02:20 +0900

    upd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AuthSession>
-          <ProtectedPage children={children} />
+          <div>점검중..</div>
         </AuthSession>
       </body>
     </html>
