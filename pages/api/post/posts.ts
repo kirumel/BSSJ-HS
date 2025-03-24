01f9fb781130162dc3f52431a702a0c56f42aa2d
kirumel <kirumel7621@gmail.com>
2025-03-24 10:05:22 +0900

    sdsdf

--- pages/api/post/posts.ts
+++ pages/api/post/posts.ts
@@ -1,6 +1,7 @@
 import { prisma } from "../prisma/lib/prisma";
 
 export default async function handler(req: any, res: any) {
+  console.log(req.method);
   if (req.method === "GET") {
     try {
       // Get boardName from query parameters
@@ -80,7 +81,6 @@ export default async function handler(req: any, res: any) {
         .json({ message: "오류가 발생했습니다 콘솔을 확인해주세요" });
     }
   } else {
-    res.setHeader("Allow", ["GET", "POST"]);
     res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
