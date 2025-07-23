cf7f90a9658fa3bba01c76df77141b4d68da601a
kirumel <kirumel7621@gmail.com>
2025-07-24 02:02:44 +0900

    test3.1

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -172,7 +172,7 @@ export default function Page() {
               {session?.user?.class}반
             </p>
             <p className="event-text" style={{ fontSize: "12px" }}>
-              공지 : 8교시 하지 않는 학생은 삭제해주세요
+              공지 : 방학 출석은 야자랑 비슷합니다 나오는 모달은 무시해주세요
             </p>
             <a href="/adminfeed">
               <button className="event-feed">feed 등록하기</button>
